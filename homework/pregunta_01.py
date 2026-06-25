ceb9ab86cd6ef91a93d18959dbeba4f8a13d9471
sacatanoc <186743736+sacatanoc@users.noreply.github.com>
2026-06-25 11:01:56 -0500

    Commit #8

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,92 @@
 Escriba el codigo que ejecute la accion solicitada.
 """
 
+import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+def load_data():
+    df = pd.read_csv("files/input/shipping-data.csv")
+    return df
+
+def create_visual_for_shipping_per_warehouse(df):
+    df = df.copy()
+    plt.figure()
+    counts = df.Warehouse_block.value_counts()
+    counts.plot.bar(
+        title = "Shipping per Warehouse",
+        xlabel = "Warehouse block",
+        ylabel = "Record Count",
+        color = "tab:blue",
+        fontsize = 8
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.savefig("docs/shipping_per_warehouse.png")
+
+def create_visual_for_mode_of_shipment(df):
+    df = df.copy()
+    plt.figure()
+    counts = df.Mode_of_Shipment.value_counts()
+    counts.plot.pie(
+        title = "Mode of shipment",
+        wedgeprops = dict(width=0.35),
+        ylabel  = "",
+        colors = ["tab:blue", "tab:orange", "tab:green"]
+    )
+    plt.savefig("docs/mode_of_shipment.png")
+
+def create_visual_for_average_customer_rating(df):
+    df = df.copy()
+    plt.figure()
+    df = (
+        df[["Mode_of_Shipment", "Customer_rating"]]
+        .groupby("Mode_of_Shipment")
+        .describe()
+    )
+    df.columns = df.columns.droplevel()
+    df = df[["mean", "min", "max"]]
+    plt.barh(
+        y = df.index.values,
+        width = df["max"].values - 1,
+        left = df["min"].values,
+        height = 0.9,
+        color = "lightgray",
+        alpha = 0.8
+    )
+    colors = [
+        "tab:green" if value >= 3.8 else "tab:orange" for value in df["mean"].values
+    ]
+    plt.barh(
+        y=df.index.values,
+        width = df["mean"].values - 1,
+        left = df["min"].values,
+        color = colors,
+        height = 0.5,
+        alpha = 1.0
+    )
+
+    plt.title("Average Customer Rating")
+    plt.gca().spines["left"].set_color("gray")
+    plt.gca().spines["bottom"].set_color("gray")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.savefig("docs/average_customer_rating.png")
+
+def create_visual_for_weight_distribution(df):
+    df = df.copy()
+    plt.figure()
+    df.Weight_in_gms.plot.hist(
+        title = "Shipped Weight Distribution",
+        color = "tab:orange",
+        edgecolor = "white"
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    plt.savefig("docs/weight_distribution.png")
 
 def pregunta_01():
     """
@@ -24,7 +110,7 @@ def pregunta_01():
 
     Para ello, siga las instrucciones dadas en el siguiente video:
 
-    https://youtu.be/AgbWALiAGVo
+    
 
     Tenga en cuenta los siguientes cambios respecto al video:
 
@@ -35,3 +121,30 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+
+    Path("docs").mkdir(parents=True, exist_ok=True)
+
+    df = load_data()
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+
+    html = """
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1>Shipping Dashboard Example</h1>
+            <div style="width:45%;float:left">
+                <img src="shipping_per_warehouse.png" alt="Fig 1">
+                <img src="mode_of_shipment.png" alt="Fig 2">
+            </div>
+            <div style="width:45%;float:left">
+                <img src="average_customer_rating.png" alt="Fig 3">
+                <img src="weight_distribution.png" alt="Fig 4">
+            </div>
+        </body>
+    </html>"""
+
+    with open("docs/index.html", "w", encoding="utf-8") as file:
+        file.write(html)
